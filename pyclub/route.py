5dfaf26d84f0775186f41ced4612b38517cb479b
akuetteh51 <akuetteh58@gmail.com>
2021-11-20 04:45:46 +0000

    new file:   __pycache__/form.cpython-39.pyc 	new file:   requirement.txt 	modified:   route.py 	modified:   static/style.css

--- pyclub/route.py
+++ pyclub/route.py
@@ -1,8 +1,16 @@
 from flask import Flask,render_template,request
 from form import Form
-from mom_api import momo_send
+import beyonic
+# from momo_api import momo_send
+
+
+
+
+
 app=Flask(__name__)
 
+app.config['SECRET_KEY']='mysecret' 
+# myapi = os.environ['BEYONIC_ACCESS_KEY']
 @app.route('/')
 def index():
     form=Form()
@@ -23,5 +31,46 @@ def success():
         Contact = form.Contact.data
         momo_send(Contact,Amount)
     return render_template('success.html')
+
+#     @app.route("/payment", methods = ['POST', 'GET'])
+# def makepayment():
+#     form=Form()
+
+#     if request.method == "POST":
+    #    if form.is_submitted():
+    #         print("Form successfully submitted")
+    #    if form.validate_on_submit():
+        # reg = form.Registration_No.data
+        # name = form.Name.data
+        # level = form.level.data
+        # email = form.Email.data
+        # momo =form.MobileMoney.data
+        # Amount = form.Amount.data
+        # Contact = form.Contact.data
+        # name = request.form.data("Name", default_value)
+        # currency="BXC"
+        # description="NEw money"
+        # callback_url='https://nhanaqwahme.pythonanywhere.com/payments/callback',
+
+        # # payment = beyonic.Payment.create(
+        #                       phonenumber = Contact,
+        #                       Name = name,
+        #                       amount = Amount,
+        #                       currency = currency,
+        #                       description = description,
+        #                       callback_url = callback_url
+        #                       #metadata={'id': '1234', 'name': 'Lucy'}
+        #                       )
+        # return 'success' 
+    #  return render_template('index.html',form=form)
+        # print(Contact)
+        # return(momo_send(Contact,Amount))
+    # return render_template('success.html')
+
+#list all your transactions
+# @app.route("/transactions")
+# def transactions ():
+#     # transactions  = beyonic.Transaction.list()
+    # return transactions
 if __name__=='__main__':
    app.run(debug=True)
